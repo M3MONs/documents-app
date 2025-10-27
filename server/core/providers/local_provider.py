f41ebce4bb112b5349d98ef3e6f41c3f114d034e
M3MONs <alan.skrrrz@gmail.com>
2025-10-27 18:48:57 +0100

    feat: refactor LocalAuthProvider to use UserService for user authentication and registration

--- server/core/providers/local_provider.py
+++ server/core/providers/local_provider.py
@@ -1,14 +1,13 @@
-from typing import cast
+from typing import Optional, cast
 from sqlalchemy.ext.asyncio import AsyncSession
-from repositories.user_repository import UserRepository
 from core.security import hash_password, verify_password
 from models.user import User
 from core.auth_base import AuthProvider
-from repositories.base_repository import BaseRepository
+from services.user_service import UserService
 
 class LocalAuthProvider(AuthProvider):
     async def authenticate(self, db: AsyncSession, username: str, password: str) -> User | None:
-        user = await UserRepository.get_by_username(db, username)
+        user = await UserService.get_user_by_username(db, username)
 
         if not user:
             return None
@@ -19,7 +18,7 @@ class LocalAuthProvider(AuthProvider):
         
         return user
 
-    async def register(self, db: AsyncSession, username: str, email: str, password: str) -> User | None:
+    async def register(self, db: AsyncSession, username: str, email: Optional[str], password: str) -> User | None:
         user = User(username=username, email=email, hashed_password=hash_password(password))
-        await BaseRepository.create(db, user)
+        await UserService.create_user(db, user)
         return user

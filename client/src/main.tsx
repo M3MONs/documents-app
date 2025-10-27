f76127eac332be482e8bbc8d24e6303f973dca5d
M3MONs <alan.skrrrz@gmail.com>
2025-10-27 20:57:19 +0100

    feat: restructure main app component and add HomePage and LoginPage components

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
+import "./index.css";
+import HomePage from "./pages/HomePage/HomePage.tsx";
+import LoginPage from "./pages/LoginPage/LoginPage.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/login" element={<LoginPage />} />
+            </Routes>
+        </BrowserRouter>
+    </StrictMode>
+);

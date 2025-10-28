c7c17fdb42bdf78ab3902d6488a375b3afeb0c38
M3MONs <alan.skrrrz@gmail.com>
2025-10-28 16:55:21 +0100

    feat: add axios API client with request interceptor for authentication

---
+++ client/src/services/apiClient.tsx
@@ -0,0 +1,26 @@
+import axios, { type InternalAxiosRequestConfig } from 'axios';
+import { getAuthToken } from './tokenManager';
+
+const apiClient = axios.create({
+  baseURL: '/api',
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  withCredentials: true,
+});
+
+apiClient.interceptors.request.use(
+  (config: InternalAxiosRequestConfig) => {
+    const token = getAuthToken();
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default apiClient;

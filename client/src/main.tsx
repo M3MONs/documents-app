963c9723781f14eea6349852dcd4ee28e1390265
M3MONs <alan.skrrrz@gmail.com>
2025-10-28 17:23:58 +0100

    feat: refactor main.tsx to include AuthProvider and ProtectedRoute for enhanced routing and authentication

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,17 +1,27 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router";
-import "./index.css";
-import HomePage from "./pages/HomePage/HomePage.tsx";
-import LoginPage from "./pages/LoginPage/LoginPage.tsx";
+import "@/index.css";
+import HomePage from "@/pages/HomePage/HomePage.tsx";
+import LoginPage from "@/pages/LoginPage/LoginPage.tsx";
+import RegisterPage from "@/pages/RegisterPage/RegisterPage.tsx";
+import { AuthProvider } from "@/context/AuthContext.tsx";
+import ProtectedRoute from "./components/ProtectedRoute";
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/login" element={<LoginPage />} />
-            </Routes>
-        </BrowserRouter>
+        <AuthProvider>
+            <BrowserRouter>
+                <Routes>
+                    <Route element={<ProtectedRoute />}>
+                        <Route path="/" element={<HomePage />} />
+                    </Route>
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="/register" element={<RegisterPage />} />
+                    <Route path="*" element={<NotFoundPage />} />
+                </Routes>
+            </BrowserRouter>
+        </AuthProvider>
     </StrictMode>
 );

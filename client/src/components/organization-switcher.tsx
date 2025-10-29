2d1b4b9369e7dbbc1ee2ccf876dde05edbe3b6f0
M3MONs <alan.skrrrz@gmail.com>
2025-10-29 18:41:25 +0100

    feat: add OrganizationSwitcher component for switching user organizations

---
+++ client/src/components/organization-switcher.tsx
@@ -0,0 +1,73 @@
+import { useState } from "react";
+import { ChevronsUpDown } from "lucide-react";
+
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuShortcut,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { SidebarMenu, SidebarMenuButton, SidebarMenuItem, useSidebar } from "@/components/ui/sidebar";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+
+export function OrganizationSwitcher() {
+    const { isMobile } = useSidebar();
+    const [organizations, _] = useState<any[] | null>(null);
+    const [activeOrganization, setActiveOrganization] = useState<any | null>(null);
+
+    return (
+        <SidebarMenu>
+            <SidebarMenuItem>
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <SidebarMenuButton
+                            size="lg"
+                            className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+                            disabled={!organizations}
+                        >
+                            <Avatar className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
+                                <AvatarImage src={activeOrganization?.logo} alt={activeOrganization?.name} />
+                                <AvatarFallback className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
+                                    {activeOrganization ? activeOrganization?.name.charAt(0) : "N"}
+                                </AvatarFallback>
+                            </Avatar>
+
+                            <div className="grid flex-1 text-left text-sm leading-tight">
+                                <span className="truncate font-medium">
+                                    {activeOrganization ? activeOrganization?.name : "No organization"}
+                                </span>
+                            </div>
+                            <ChevronsUpDown className="ml-auto" />
+                        </SidebarMenuButton>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent
+                        className="w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg"
+                        align="start"
+                        side={isMobile ? "bottom" : "right"}
+                        sideOffset={4}
+                    >
+                        <DropdownMenuLabel className="text-muted-foreground text-xs">Organizations</DropdownMenuLabel>
+                        {organizations?.map((organization, index) => (
+                            <DropdownMenuItem
+                                key={organization.name}
+                                onClick={() => setActiveOrganization(organization)}
+                                className="gap-2 p-2"
+                            >
+                                <div className="flex size-6 items-center justify-center rounded-md border">
+                                    <Avatar>
+                                        <AvatarImage src={organization.logo} alt={organization.name} />
+                                        <AvatarFallback>{organization.name.charAt(0)}</AvatarFallback>
+                                    </Avatar>
+                                </div>
+                                {organization.name}
+                                <DropdownMenuShortcut>⌘{index + 1}</DropdownMenuShortcut>
+                            </DropdownMenuItem>
+                        ))}
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            </SidebarMenuItem>
+        </SidebarMenu>
+    );
+}

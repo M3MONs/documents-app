7f1837665a6fddec8fddd0e71d4243eef78f202a
M3MONs <alan.skrrrz@gmail.com>
2025-10-29 18:40:59 +0100

    feat: add NavUser component for user profile and logout functionality

---
+++ client/src/components/nav-user.tsx
@@ -0,0 +1,94 @@
+import { ChevronsUpDown, LogOut, SunMoon } from "lucide-react";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { SidebarMenu, SidebarMenuButton, SidebarMenuItem, useSidebar } from "@/components/ui/sidebar";
+import { useTheme } from "@/components/theme-provider";
+import { useAuth } from "@/context/AuthContext";
+import AuthService from "@/services/authService";
+import { useNavigate } from "react-router";
+
+export function NavUser() {
+    const { isMobile } = useSidebar();
+    const { user, setToken } = useAuth();
+    const { handleSwitchTheme } = useTheme();
+    const navigate = useNavigate();
+
+    const handleLogout = async () => {
+        try{
+          await AuthService.logout();
+          setToken(null);
+          navigate("/login");
+        }catch (err: any) {
+            console.error("Logout failed:", err.message);
+            setToken(null);
+            navigate("/login");
+            
+        }
+    }
+
+    return (
+        <SidebarMenu>
+            <SidebarMenuItem>
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <SidebarMenuButton
+                            size="lg"
+                            className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+                        >
+                            <Avatar className="h-8 w-8 rounded-lg">
+                                <AvatarImage src={""} alt={user?.username} />
+                                <AvatarFallback className="rounded-lg">
+                                    {user?.username[0].toUpperCase()}
+                                </AvatarFallback>
+                            </Avatar>
+                            <div className="grid flex-1 text-left text-sm leading-tight">
+                                <span className="truncate font-medium">{user?.username}</span>
+                                <span className="truncate text-xs">{user?.email}</span>
+                            </div>
+                            <ChevronsUpDown className="ml-auto size-4" />
+                        </SidebarMenuButton>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent
+                        className="w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg"
+                        side={isMobile ? "bottom" : "right"}
+                        align="end"
+                        sideOffset={4}
+                    >
+                        <DropdownMenuLabel className="p-0 font-normal">
+                            <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+                                <Avatar className="h-8 w-8 rounded-lg">
+                                    <AvatarImage src={""} alt={user?.username} />
+                                    <AvatarFallback className="rounded-lg">
+                                        {user?.username[0].toUpperCase()}
+                                    </AvatarFallback>
+                                </Avatar>
+                                <div className="grid flex-1 text-left text-sm leading-tight">
+                                    <span className="truncate font-medium">{user?.username}</span>
+                                    <span className="truncate text-xs">{user?.email}</span>
+                                </div>
+                            </div>
+                        </DropdownMenuLabel>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem onSelect={handleSwitchTheme}>
+                            <SunMoon />
+                            Switch Theme
+                        </DropdownMenuItem>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem onSelect={handleLogout}>
+                            <LogOut />
+                            Log out
+                        </DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            </SidebarMenuItem>
+        </SidebarMenu>
+    );
+}

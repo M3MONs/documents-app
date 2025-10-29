8658f412710d089d26070ce1bae2cf00351b5432
M3MONs <alan.skrrrz@gmail.com>
2025-10-29 18:42:11 +0100

    feat: implement ThemeProvider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,22 +6,25 @@ import HomePage from "@/pages/HomePage/HomePage.tsx";
 import LoginPage from "@/pages/LoginPage/LoginPage.tsx";
 import RegisterPage from "@/pages/RegisterPage/RegisterPage.tsx";
 import { AuthProvider } from "@/context/AuthContext.tsx";
-import ProtectedRoute from "./components/ProtectedRoute";
-import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
+import ProtectedRoute from "@/components/ProtectedRoute";
+import NotFoundPage from "@/pages/NotFoundPage/NotFoundPage";
+import { ThemeProvider } from "@/components/theme-provider";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <AuthProvider>
-            <BrowserRouter>
-                <Routes>
-                    <Route element={<ProtectedRoute />}>
-                        <Route path="/" element={<HomePage />} />
-                    </Route>
-                    <Route path="/login" element={<LoginPage />} />
-                    <Route path="/register" element={<RegisterPage />} />
-                    <Route path="*" element={<NotFoundPage />} />
-                </Routes>
-            </BrowserRouter>
-        </AuthProvider>
+        <ThemeProvider>
+            <AuthProvider>
+                <BrowserRouter>
+                    <Routes>
+                        <Route element={<ProtectedRoute />}>
+                            <Route path="/" element={<HomePage />} />
+                        </Route>
+                        <Route path="/login" element={<LoginPage />} />
+                        <Route path="/register" element={<RegisterPage />} />
+                        <Route path="*" element={<NotFoundPage />} />
+                    </Routes>
+                </BrowserRouter>
+            </AuthProvider>
+        </ThemeProvider>
     </StrictMode>
 );

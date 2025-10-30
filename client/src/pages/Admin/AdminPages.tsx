d622a849534e686e23e8ea0f98d376fb506fe29d
M3MONs <alan.skrrrz@gmail.com>
2025-10-30 17:40:30 +0100

    feat: add AdminPages component with protected routes for admin sections

---
+++ client/src/pages/Admin/AdminPages.tsx
@@ -0,0 +1,22 @@
+import { Route, Routes } from "react-router";
+import DashboardLayout from "@/components/layouts/DashboardLayout"
+import ProtectedRoute from "@/components/ProtectedRoute";
+import AdminUsersPage from "@/pages/Admin/UsersPage/AdminUsersPage";
+import AdminDepartmentsPage from "@/pages/Admin/DepartmentsPage/AdminDepartmentsPage";
+import AdminOrganizationsPage from "@/pages/Admin/OrganizationsPage/AdminOrganizationsPage";
+
+const AdminPages = () => {
+    return (
+        <DashboardLayout>
+            <Routes>
+                <Route element={<ProtectedRoute isSuperuser={true} />}>
+                    <Route path="users" element={<AdminUsersPage />} />
+                    <Route path="organizations" element={<AdminOrganizationsPage />} />
+                    <Route path="departments" element={<AdminDepartmentsPage />} />
+                </Route>
+            </Routes>
+        </DashboardLayout>
+    );
+};
+
+export default AdminPages;

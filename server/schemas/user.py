13cb3fa5044f89606be02d08dfdef1d3dac13f72
M3MONs <alan.skrrrz@gmail.com>
2025-10-30 17:29:54 +0100

    feat: create User schema model

---
+++ server/schemas/user.py
@@ -0,0 +1,17 @@
+import uuid
+from pydantic import BaseModel
+from schemas.role import Role
+from schemas.organization import Organization
+
+class User(BaseModel):
+    id: uuid.UUID
+    username: str
+    email: str | None = None
+    is_active: bool
+    is_superuser: bool | None = None
+    role: Role | None = None
+    primary_organization: Organization | None = None
+    additional_organizations: list[Organization] = []
+    
+    class Config:
+        from_attributes = True

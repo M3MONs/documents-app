c8a3de0f0a3454b704d28d5e417ed71e4e47d67c
M3MONs <alan.skrrrz@gmail.com>
2025-10-30 17:39:27 +0100

    feat: implement dynamic rendering of admin pages in NavAdmin component

--- client/src/components/nav-admin.tsx
+++ client/src/components/nav-admin.tsx
@@ -1,10 +1,35 @@
-import { SidebarGroup, SidebarGroupLabel, SidebarMenu } from "@/components/ui/sidebar";
+import {
+    SidebarGroup,
+    SidebarGroupLabel,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import { Building2, Group, User } from "lucide-react";
+import { NavLink } from "react-router";
+
+const ADMIN_PAGES = [
+    { name: "Users", path: "/admin/users", icon: User },
+    { name: "Organizations", path: "/admin/organizations", icon: Building2 },
+    { name: "Departments", path: "/admin/departments", icon: Group },
+];
 
 export function NavAdmin() {
     return (
         <SidebarGroup className="group-data-[collapsible=icon]:hidden">
             <SidebarGroupLabel>Admin</SidebarGroupLabel>
-            <SidebarMenu>{/* TODO: List of admin pages */}</SidebarMenu>
+            <SidebarMenu>
+                {ADMIN_PAGES.map((page) => (
+                    <SidebarMenuItem key={page.path}>
+                        <SidebarMenuButton asChild>
+                            <NavLink to={page.path} className="flex items-center gap-2">
+                                <page.icon />
+                                {page.name}
+                            </NavLink>
+                        </SidebarMenuButton>
+                    </SidebarMenuItem>
+                ))}
+            </SidebarMenu>
         </SidebarGroup>
     );
 }

0a4826fd2dbca7d19f92ecf9ba2bc7ddaf3f7ca9
M3MONs <alan.skrrrz@gmail.com>
2025-10-30 17:30:47 +0100

    feat: extend User interface to include superuser and organization fields

--- client/src/types/user.tsx
+++ client/src/types/user.tsx
@@ -1,5 +1,12 @@
+import type { Organization } from "./organization";
+import type { Role } from "./role";
+
 export interface User {
     id: number;
     username: string;
     email?: string | null;
+    is_superuser?: boolean | null;
+    role?: Role | null;
+    primary_organization?: Organization | null;
+    additional_organizations?: Organization[] | null;
 }

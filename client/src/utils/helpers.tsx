3936cfdffad84106891ae29c7cf8dd80ff5e354c
M3MONs <alan.skrrrz@gmail.com>
2025-11-01 16:26:08 +0100

    feat: add capitalize helper function for string formatting

---
+++ client/src/utils/helpers.tsx
@@ -0,0 +1,8 @@
+export const capitalize = (str: string) => {
+    if (!str || typeof str !== "string" || str.length === 0) {
+        return "";
+    }
+
+    const restOfString = str.slice(1).toLowerCase();
+    return str.charAt(0).toUpperCase() + restOfString;
+};

0732ca4b63b2c0087edfc9d9d53be14d32b0f7d7
M3MONs <alan.skrrrz@gmail.com>
2025-11-01 17:52:32 +0100

    fix: change User id type from number to string for consistency

--- client/src/types/user.tsx
+++ client/src/types/user.tsx
@@ -2,9 +2,10 @@ import type { Organization } from "./organization";
 import type { Role } from "./role";
 
 export interface User {
-    id: number;
+    id: string;
     username: string;
     email?: string | null;
+    is_active: boolean;
     is_superuser?: boolean | null;
     role?: Role | null;
     primary_organization?: Organization | null;

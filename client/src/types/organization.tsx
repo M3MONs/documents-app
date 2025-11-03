b41217c548c414f71eef9a19a02c9b7c15868dc8
M3MONs <alan.skrrrz@gmail.com>
2025-11-03 18:58:28 +0100

    feat: add OrganizationCreatePayload + update Organization

--- client/src/types/organization.tsx
+++ client/src/types/organization.tsx
@@ -1,5 +1,12 @@
 export interface Organization {
     id: string;
     name: string;
-    description?: string | null;
+    domain?: string | null;
+    is_active: boolean;    
+}
+
+export interface OrganizationCreatePayload {
+    name: string;
+    domain?: string | null;
+    is_active: boolean;
 }

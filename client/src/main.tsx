a2ca06ae36525a8b41c6b675b71388b64f4a1b93
M3MONs <alan.skrrrz@gmail.com>
2025-11-03 17:10:16 +0100

    feat: add Toaster to app for toasts

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -11,27 +11,29 @@ import NotFoundPage from "@/pages/NotFoundPage/NotFoundPage";
 import { ThemeProvider } from "@/components/theme-provider";
 import AdminPages from "@/pages/Admin/AdminPages";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/sonner";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <ThemeProvider>
             <QueryClientProvider client={new QueryClient()}>
-            <AuthProvider>
-                <BrowserRouter>
-                    <Routes>
-                        <Route element={<ProtectedRoute />}>
-                            <Route path="/" element={<HomePage />} />
-                        </Route>
+                <AuthProvider>
+                    <BrowserRouter>
+                        <Routes>
+                            <Route element={<ProtectedRoute />}>
+                                <Route path="/" element={<HomePage />} />
+                            </Route>
 
-                        <Route path="/admin/*" element={<AdminPages />} />
+                            <Route path="/admin/*" element={<AdminPages />} />
 
-                        <Route path="/login" element={<LoginPage />} />
-                        <Route path="/register" element={<RegisterPage />} />
-                        <Route path="*" element={<NotFoundPage />} />
-                    </Routes>
-                </BrowserRouter>
-            </AuthProvider>
+                            <Route path="/login" element={<LoginPage />} />
+                            <Route path="/register" element={<RegisterPage />} />
+                            <Route path="*" element={<NotFoundPage />} />
+                        </Routes>
+                    </BrowserRouter>
+                </AuthProvider>
             </QueryClientProvider>
+            <Toaster />
         </ThemeProvider>
     </StrictMode>
 );

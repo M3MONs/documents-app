0c2f83e483553cbca087cd990e1fbbf59cdada7e
M3MONs <alan.skrrrz@gmail.com>
2025-11-03 17:37:19 +0100

    feat: implement EditUser component with tabs for user details and password change

---
+++ client/src/pages/Admin/UsersPage/components/EditUser.tsx
@@ -0,0 +1,65 @@
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogClose, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs"; // Dodaj import Tabs
+import type { User } from "@/types/user";
+import ChangePasswordTab from "./tabs/ChangePasswordTab";
+import EditUserTab from "./tabs/EditUserTab";
+import { useRef, useState } from "react";
+
+interface EditUserProps {
+    user?: User;
+    isOpen: boolean;
+    onClose: () => void;
+    onConfirm: () => void;
+}
+
+const EditUser = ({ user, isOpen, onClose, onConfirm }: EditUserProps) => {
+    if (!user) return null;
+
+    const [activeTab, setActiveTab] = useState("details");
+    const editUserRef = useRef<any>(null);
+    const changePasswordRef = useRef<any>(null);
+
+    const handleSave = () => {
+        if (activeTab === "details") {
+            editUserRef.current?.submit();
+        } else if (activeTab === "password") {
+            changePasswordRef.current?.submit();
+        }
+        onConfirm();
+    };
+
+    return (
+        <Dialog open={isOpen} onOpenChange={onClose}>
+            <DialogContent aria-describedby={undefined}>
+                <DialogHeader>
+                    <DialogTitle>Edit User</DialogTitle>
+                </DialogHeader>
+                <div>
+                    <Tabs value={activeTab} onValueChange={setActiveTab} className="w-full">
+                        <TabsList className="grid w-full grid-cols-2">
+                            <TabsTrigger value="details">User Details</TabsTrigger>
+                            <TabsTrigger value="password">Change Password</TabsTrigger>
+                        </TabsList>
+
+                        <EditUserTab ref={editUserRef} user={user} />
+                        <ChangePasswordTab ref={changePasswordRef} user={user} />
+                    </Tabs>
+
+                    <div className="mt-4 flex justify-between gap-2">
+                        <DialogClose asChild>
+                            <Button type="button" variant="secondary">
+                                Cancel
+                            </Button>
+                        </DialogClose>
+                        <Button onClick={handleSave} type="button">
+                            Save Changes
+                        </Button>
+                    </div>
+                </div>
+            </DialogContent>
+        </Dialog>
+    );
+};
+
+export default EditUser;

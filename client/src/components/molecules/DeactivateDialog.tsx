30ab90b97947c25957f5bf16bbab6506419e4bea
M3MONs <alan.skrrrz@gmail.com>
2025-11-04 16:55:38 +0100

    feat: enhance DeactivateDialog to accept title and description props

--- client/src/components/molecules/DeactivateDialog.tsx
+++ client/src/components/molecules/DeactivateDialog.tsx
@@ -4,19 +4,20 @@ import { Button } from "@/components/ui/button";
 interface DeactivateDialogProps {
     isOpen: boolean;
     text?: string;
+    title?: string;
+    description?: string;
     onClose: () => void;
     onConfirm: () => void;
 }
 
-const DeactivateDialog = ({ isOpen, text, onClose, onConfirm }: DeactivateDialogProps) => {
+const DeactivateDialog = ({ isOpen, text, title, description, onClose, onConfirm }: DeactivateDialogProps) => {
     return (
         <Dialog open={isOpen} onOpenChange={onClose}>
             <DialogContent>
                 <DialogHeader>
-                    <DialogTitle>Deactivate</DialogTitle>
+                    <DialogTitle>{title ? title : "Deactivate"}</DialogTitle>
                     <DialogDescription>
-                        This action will change the status of the {text ? text : "item"} to inactive. Are you sure you
-                        want to proceed?
+                        {description ? description : `This action will change the status of the ${text ? text : "item"} to inactive. Are you sure you want to proceed?`}
                     </DialogDescription>
                 </DialogHeader>
                 <DialogFooter className="mt-4 flex justify-end gap-2">

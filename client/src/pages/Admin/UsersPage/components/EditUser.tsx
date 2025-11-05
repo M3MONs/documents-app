aee974d1ee201395b169aef0a2df574dbbf8aa39
M3MONs <alan.skrrrz@gmail.com>
2025-11-05 21:49:41 +0100

    feat: add OrganizationsTab to EditUser component for managing user organizations

--- client/src/pages/Admin/UsersPage/components/EditUser.tsx
+++ client/src/pages/Admin/UsersPage/components/EditUser.tsx
@@ -4,6 +4,7 @@ import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs"; // Dodaj imp
 import type { User } from "@/types/user";
 import ChangePasswordTab from "./tabs/ChangePasswordTab";
 import EditUserTab from "./tabs/EditUserTab";
+import OrganizationsTab from "./tabs/OrganizationsTab";
 import { useRef, useState } from "react";
 
 interface EditUserProps {
@@ -19,31 +20,39 @@ const EditUser = ({ user, isOpen, onClose, onConfirm }: EditUserProps) => {
     const [activeTab, setActiveTab] = useState("details");
     const editUserRef = useRef<any>(null);
     const changePasswordRef = useRef<any>(null);
+    const organizationRef = useRef<any>(null);
 
     const handleSave = async () => {
         if (activeTab === "details") {
             await editUserRef.current?.submit();
         } else if (activeTab === "password") {
             await changePasswordRef.current?.submit();
+        } else if (activeTab === "organization") {
+            await organizationRef.current?.submit();
         }
         onConfirm();
     };
 
     return (
         <Dialog open={isOpen} onOpenChange={onClose}>
-            <DialogContent aria-describedby={undefined}>
+            <DialogContent
+                aria-describedby={undefined}
+                className="max-w-[95vw] md:max-w-2xl lg:max-w-3xl px-4 flex flex-col"
+            >
                 <DialogHeader>
                     <DialogTitle>Edit User</DialogTitle>
                 </DialogHeader>
                 <div>
                     <Tabs value={activeTab} onValueChange={setActiveTab} className="w-full">
-                        <TabsList className="grid w-full grid-cols-2">
+                        <TabsList className="grid w-full grid-cols-1 sm:grid-cols-3 h-auto gap-2 mb-4">
                             <TabsTrigger value="details">User Details</TabsTrigger>
                             <TabsTrigger value="password">Change Password</TabsTrigger>
+                            <TabsTrigger value="organization">Organizations</TabsTrigger>
                         </TabsList>
 
                         <EditUserTab ref={editUserRef} user={user} />
                         <ChangePasswordTab ref={changePasswordRef} user={user} />
+                        <OrganizationsTab ref={organizationRef} user={user} />
                     </Tabs>
 
                     <div className="mt-4 flex justify-between gap-2">

af2f0c874b1af50099a3a9980ad77afcd020b19d
M3MONs <alan.skrrrz@gmail.com>
2025-11-10 18:58:12 +0100

    feat: enhance lifespan function to create roles during application startup

--- server/main.py
+++ server/main.py
@@ -2,16 +2,24 @@ from contextlib import asynccontextmanager
 import logging
 from fastapi import APIRouter, FastAPI, Request, status
 from fastapi.responses import JSONResponse
-from core.database import engine, Base
+from core.database import engine, Base, AsyncSessionLocal
 from models import organization, department, role, user  # noqa: F401
 from routes import auth
 from routes.admin import admin_user, admin_organization, admin_department, admin_role, admin_user_organization_role
+from core.roles import StaticRole
+from schemas.role import RoleCreatePayload
+from services.role_service import RoleService
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
+    async with AsyncSessionLocal() as db:
+        for role_data in StaticRole.all_roles():
+            if await RoleService.is_unique_name(db, role_data["name"]):
+                payload = RoleCreatePayload(**role_data)
+                await RoleService.create_role(db, payload)
     yield
 
 

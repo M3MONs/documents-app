f8600e8952c70cbb8da5a57b9a33650c08d3b128
M3MONs <alan.skrrrz@gmail.com>
2025-11-13 18:36:20 +0100

    feat: add route for AdminCategoriesPage with role-based access control

--- client/src/pages/Admin/AdminPages.tsx
+++ client/src/pages/Admin/AdminPages.tsx
@@ -1,11 +1,12 @@
 import { Route, Routes } from "react-router";
-import DashboardLayout from "@/components/layouts/DashboardLayout"
+import DashboardLayout from "@/components/layouts/DashboardLayout";
 import ProtectedRoute from "@/components/ProtectedRoute";
 import AdminUsersPage from "@/pages/Admin/UsersPage/AdminUsersPage";
 import AdminDepartmentsPage from "@/pages/Admin/DepartmentsPage/AdminDepartmentsPage";
 import AdminOrganizationsPage from "@/pages/Admin/OrganizationsPage/AdminOrganizationsPage";
 import AdminRolesPage from "./RolesPage/AdminRolesPage";
 import { StaticRoles } from "@/constants/roles";
+import AdminCategoriesPage from "./CategoriesPage/AdminCategoriesPage";
 
 const AdminPages = () => {
     return (
@@ -22,6 +23,9 @@ const AdminPages = () => {
                 </Route>
                 <Route element={<ProtectedRoute isSuperuser />}>
                     <Route path="roles" element={<AdminRolesPage />} />
+                </Route>
+                <Route element={<ProtectedRoute requiredRoles={[StaticRoles.CATEGORIES_MANAGER.name]} />}>
+                    <Route path="categories" element={<AdminCategoriesPage />} />
                 </Route>
             </Routes>
         </DashboardLayout>

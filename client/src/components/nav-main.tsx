79a4d0a167145a89151be6d97555810e0a4974e8
M3MONs <alan.skrrrz@gmail.com>
2025-11-13 18:39:20 +0100

    feat: implement category fetching in NavMain component

--- client/src/components/nav-main.tsx
+++ client/src/components/nav-main.tsx
@@ -1,16 +1,46 @@
 import {
-  SidebarGroup,
-  SidebarGroupLabel,
-  SidebarMenu,
-} from "@/components/ui/sidebar"
+    SidebarGroup,
+    SidebarGroupLabel,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import { useAuth } from "@/context/AuthContext";
+import CategoryService from "@/services/categoryService";
+import { handleApiError } from "@/utils/errorHandler";
+import { useEffect, useState } from "react";
+import { NavLink } from "react-router";
 
 export function NavMain() {
-  return (
-    <SidebarGroup>
-      <SidebarGroupLabel>Categories</SidebarGroupLabel>
-      <SidebarMenu>
-        {/* TODO: List of main document categories */}
-      </SidebarMenu>
-    </SidebarGroup>
-  )
+    const { user, selectedOrganization } = useAuth();
+    const [categories, setCategories] = useState<Array<any>>([]);
+
+    useEffect(() => {
+        const fetchCategories = async () => {
+            try {
+                const data = await CategoryService.getCategories(selectedOrganization!.id);
+                setCategories(data);
+            } catch (err: any) {
+                handleApiError(err);
+            }
+        };
+
+        if (!user?.id || !selectedOrganization?.id) return;
+        fetchCategories();
+    }, [user?.id, selectedOrganization?.id]);
+
+    return (
+        <SidebarGroup>
+            <SidebarGroupLabel>Categories</SidebarGroupLabel>
+            <SidebarMenu>
+                {categories.map((category) => (
+                    <SidebarMenuItem key={category.id}>
+                        <SidebarMenuButton asChild>
+                            <NavLink to={`/categories/${category.id}`}>{category.name}</NavLink>
+                        </SidebarMenuButton>
+                    </SidebarMenuItem>
+                ))}
+            </SidebarMenu>
+        </SidebarGroup>
+    );
 }

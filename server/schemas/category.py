d677dfd618b9eb95b971c8e35d34adf276145802
M3MONs <alan.skrrrz@gmail.com>
2025-11-13 18:28:22 +0100

    feat: update Category model to use UUID for id and include organization relationship

--- server/schemas/category.py
+++ server/schemas/category.py
@@ -1,10 +1,18 @@
+import uuid
 from pydantic import BaseModel
 
+from schemas.organization import Organization
 
-class Category(BaseModel):
-    id: str
+class CategoryCreatePayload(BaseModel):
     name: str
     description: str
+    organization_id: str
+
+class Category(BaseModel):
+    id: uuid.UUID
+    name: str
+    description: str
+    organization: Organization
 
     class Config:
         from_attributes = True

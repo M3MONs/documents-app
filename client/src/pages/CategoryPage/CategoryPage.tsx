70a1fd3edefbbf7738d26307f8a71aae2ea7b480
M3MONs <alan.skrrrz@gmail.com>
2025-11-13 18:41:54 +0100

    feat: create CategoryPage component with DashboardLayout

---
+++ client/src/pages/CategoryPage/CategoryPage.tsx
@@ -0,0 +1,7 @@
+import DashboardLayout from "@/components/layouts/DashboardLayout";
+
+const CategoryPage = () => {
+    return <DashboardLayout>CategoryPage</DashboardLayout>;
+};
+
+export default CategoryPage;

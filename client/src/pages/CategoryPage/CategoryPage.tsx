f27d9f99655df41c61c8f66ce2d4a45200858c6d
M3MONs <alan.skrrrz@gmail.com>
2025-11-15 13:19:11 +0100

    feat: display SelectCategoryInfo when categoryId is not provided in CategoryPage

--- client/src/pages/CategoryPage/CategoryPage.tsx
+++ client/src/pages/CategoryPage/CategoryPage.tsx
@@ -1,6 +1,14 @@
+import SelectCategoryInfo from "@/components/atoms/SelectCategoryInfo";
 import DashboardLayout from "@/components/layouts/DashboardLayout";
+import { useParams } from "react-router";
 
 const CategoryPage = () => {
+    const { categoryId } = useParams<{ categoryId: string }>();
+
+    if (!categoryId) {
+        return <SelectCategoryInfo />;
+    }
+
     return <DashboardLayout>CategoryPage</DashboardLayout>;
 };
 

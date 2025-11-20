5361187695d7d31dc85142ba54235c58a54ea5cd
M3MONs <alan.skrrrz@gmail.com>
2025-11-20 18:25:19 +0100

    feat: add DocumentDialog component for displaying selected document details

--- client/src/pages/CategoryPage/components/Document/DocumentDialog.tsx
+++ client/src/pages/CategoryPage/components/Document/DocumentDialog.tsx
@@ -2,6 +2,7 @@ import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/u
 import type { ContentItem } from "@/types/categoryContent";
 import { FileText } from "lucide-react";
 import React from "react";
+import DocumentViewer from "./DocumentViewer";
 
 interface DocumentDialogProps {
     selectedDocument: ContentItem | null;
@@ -11,7 +12,7 @@ interface DocumentDialogProps {
 const DocumentDialog: React.FC<DocumentDialogProps> = ({ selectedDocument, setSelectedDocument }) => {
     return (
         <Dialog open={!!selectedDocument} onOpenChange={(open) => !open && setSelectedDocument(null)}>
-            <DialogContent className="max-w-4xl w-full max-h-screen overflow-y-auto">
+            <DialogContent className="sm:max-w-none sm:w-screen sm:h-screen md:max-w-[80vw] md:max-h-[90vh] md:w-[80vw] md:h-[90vh] overflow-hidden flex flex-col">
                 <DialogHeader>
                     <DialogTitle className="flex items-center gap-2">
                         <FileText className="h-5 w-5 text-primary" />
@@ -19,6 +20,7 @@ const DocumentDialog: React.FC<DocumentDialogProps> = ({ selectedDocument, setSe
                     </DialogTitle>
                 </DialogHeader>
 
+                <div className="flex-1 overflow-y-auto">{selectedDocument && <DocumentViewer document={selectedDocument} />}</div>
             </DialogContent>
         </Dialog>
     );

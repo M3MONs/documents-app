4992f163f1038dd756082af6c7c7b3e6a561f96b
Alan Skrzypczak <alan.skrrrz@gmail.com>
2025-12-02 19:44:17 +0100

    feat: enhance CategoryHeader to support document addition and improve layout

--- client/src/pages/CategoryPage/components/CategoryHeader.tsx
+++ client/src/pages/CategoryPage/components/CategoryHeader.tsx
@@ -2,41 +2,46 @@ import { memo, type RefObject } from "react";
 import SearchBar from "@/components/atoms/SearchBar";
 import CategoryBreadcrumb from "./CategoryBreadcrumb";
 import type { BreadcrumbItemData } from "@/types/categoryContent";
+import { Button } from "@/components/ui/button";
 
 interface CategoryHeaderProps {
-    folderHistory: BreadcrumbItemData[];
-    searchQuery: string;
-    isFocused: boolean;
-    onSearchChange: (query: string) => void;
-    onFocusChange: (focused: boolean) => void;
-    onBreadcrumbClick: (index: number) => void;
-    inputRef: RefObject<HTMLInputElement | null>;
+  folderHistory: BreadcrumbItemData[];
+  searchQuery: string;
+  isFocused: boolean;
+  canAddDocument?: boolean;
+  onAddDocument?: () => void;
+  onSearchChange: (query: string) => void;
+  onFocusChange: (focused: boolean) => void;
+  onBreadcrumbClick: (index: number) => void;
+  inputRef: RefObject<HTMLInputElement | null>;
 }
 
 const CategoryHeader = memo(
-    ({
-        folderHistory,
-        searchQuery,
-        onSearchChange,
-        onFocusChange,
-        onBreadcrumbClick,
-        inputRef,
-    }: CategoryHeaderProps) => {
-        return (
-            <div className="flex flex-col gap-4">
-                <CategoryBreadcrumb
-                    folderHistory={folderHistory}
-                    handleBreadcrumbClick={onBreadcrumbClick}
-                />
-                <SearchBar
-                    searchQuery={searchQuery}
-                    setSearchQuery={onSearchChange}
-                    setIsFocused={onFocusChange}
-                    inputRef={inputRef}
-                />
-            </div>
-        );
-    }
+  ({
+    folderHistory,
+    searchQuery,
+    canAddDocument,
+    onAddDocument,
+    onSearchChange,
+    onFocusChange,
+    onBreadcrumbClick,
+    inputRef,
+  }: CategoryHeaderProps) => {
+    return (
+      <div className="flex flex-col gap-4">
+        <CategoryBreadcrumb folderHistory={folderHistory} handleBreadcrumbClick={onBreadcrumbClick} />
+        <div className="flex gap-4">
+          <SearchBar
+            searchQuery={searchQuery}
+            setSearchQuery={onSearchChange}
+            setIsFocused={onFocusChange}
+            inputRef={inputRef}
+          />
+          {canAddDocument && <Button onClick={onAddDocument}>Add Document</Button>}
+        </div>
+      </div>
+    );
+  }
 );
 
 CategoryHeader.displayName = "CategoryHeader";

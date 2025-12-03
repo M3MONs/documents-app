16663a66b783b3afff600e8afc379c3945bfab6d
Alan Skrzypczak <alan.skrrrz@gmail.com>
2025-12-03 20:01:46 +0100

    fix: remove unused uuid import and clean up DocumentMetadata class

--- server/schemas/document.py
+++ server/schemas/document.py
@@ -1,5 +1,4 @@
 from datetime import datetime
-import uuid
 from pydantic import BaseModel
 
 
@@ -17,9 +16,3 @@ class DocumentMetadata(BaseModel):
     updated_at: datetime
     file_exists: bool
     viewable: bool
-
-
-class DocumentCreatePayload(BaseModel):
-    name: str
-    folder_id: uuid.UUID | None = None
-    organization_id: uuid.UUID

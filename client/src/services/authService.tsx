2f353601f3a9feb0c40f4c47767c11477da521b6
Alan Skrzypczak <alan.skrrrz@gmail.com>
2026-01-20 17:18:42 +0100

    feat: add type definitions for login and register methods in AuthService

--- client/src/services/authService.tsx
+++ client/src/services/authService.tsx
@@ -17,13 +17,23 @@ export default class AuthService {
         return response.data;
     };
 
-    static login = async (data: any) => {
+    static login = async (data: { username: string; password: string }) => {
         const response = await apiClient.post(`${URL}/login`, data);
         return response.data;
     };
 
-    static register = async (data: any) => {
+    static register = async (data: { username: string; email?: string | null; password: string }) => {
         const response = await apiClient.post(`${URL}/register`, data);
+        return response.data;
+    };
+
+    static updateEmail = async (data: { email: string }) => {
+        const response = await apiClient.put(`${URL}/update-email`, data);
+        return response.data;
+    };
+
+    static changePassword = async (data: { current_password: string; new_password: string }) => {
+        const response = await apiClient.put(`${URL}/change-password`, data);
         return response.data;
     };
 }
